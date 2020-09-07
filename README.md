d0130d340986f2b76d43fa62942cc4f8bd76901f
Andy <andyfg87@gmail.com>
2020-09-07 15:21:07 -0400

    Include client React app inner server directory

--- README.md
+++
@@ -1,9 +0,0 @@
-# chat-app
-
-This a simple chat channel
-
-if you can help me with with project. You are free to make changes.
-
-download the project and run "npm install" in root project and server and client directory
-
-finaly npm start in a root project
---
+++ client/build/precache-manifest.79082b6c82b7e35128da0ee1ccf78466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005203eb5f7a9c82625dbf3bc82c627f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e26420a73693f4c450",
+    "url": "/static/css/main.5085a2df.chunk.css"
+  },
+  {
+    "revision": "1ee21da21d8a7245b0c1",
+    "url": "/static/js/2.15bb9a57.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.15bb9a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e26420a73693f4c450",
+    "url": "/static/js/main.771bd76e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
